dd72f8a34b8624fd38cb6841649eb98b187d9faf
tkralj <tomislav2901@gmail.com>
2025-07-08 19:07:07 +0200

    Rijesen ispit

---
+++ view/editTodo.view.php
@@ -0,0 +1,18 @@
+<?php require_once "index.view.php"; ?>
+
+<div class="container mt-4">
+  <h2>Uredi zadatak</h2>
+  <form action="/parcijalni-php/index.php?page=update" method="POST">
+    <input type="hidden" name="id" value="<?= htmlspecialchars($todo['id']) ?>" />
+    <div class="mb-3">
+      <label for="naslov" class="form-label">Naslov</label>
+      <input type="text" id="naslov" name="naslov" class="form-control" value="<?= htmlspecialchars($todo['naslov']) ?>" required />
+    </div>
+    <div class="mb-3">
+      <label for="opis" class="form-label">Opis</label>
+      <textarea id="opis" name="opis" class="form-control" rows="4"><?= htmlspecialchars($todo['opis']) ?></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Spremi promjene</button>
+    <a href="/parcijalni-php/index.php?page=index" class="btn btn-secondary ms-2">Odustani</a>
+  </form>
+</div>
