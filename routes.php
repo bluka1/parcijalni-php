1b3fc57148d1aee067b24cbc44d5ad0778712864
sasa <sasa.franic@gmail.com>
2025-07-08 19:43:18 +0200

    SF

---
+++ routes.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    '/' => 'homeController.php',
+    '/zadaci' => 'zadaci/index.php',
+    '/zadaci-kreiraj' => 'zadaci/create.php',
+    '/zadaci-uredi' => 'zadaci/edit.php',
+    '/zadaci-izbriši' => 'zadaci/delete.php',
+];
